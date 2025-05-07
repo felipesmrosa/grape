dd4c09d4328e2d02e938edb30dbfc5dd298c9d81
Felipe Miranda <felipem04.r@gmail.com>
2025-05-07 08:56:41 -0300

    Update Index.jsx

--- grape/src/layout/cabecalho/Index.jsx
+++ grape/src/layout/cabecalho/Index.jsx
@@ -18,7 +18,7 @@ export function Cabecalho() {
                 dinheiro
               </h1>
               <p>Participe dos jogos mais populares e ganhe muito.</p>
-              <button>Teste Vercel Privado</button>
+              <button>Participar</button>
             </span>
           </div>
         </div>
